abec21306a96eb7f5846114f588b478ec8f01c6b
Pedro Dib <pedrohenrique635@hotmail.com>
2018-02-01 13:55:10 -0200

    #8 Implementing Player Tests
    Tests of PlayerController and SteamComponent

---
+++ api/tests/Fixture/PlayerFixture.php
@@ -0,0 +1,51 @@
+<?php 
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class PlayerFixture extends TestFixture
+{
+      // Optional. Set this property to load fixtures to a different test datasource
+      public $connection = 'test';
+
+      public $fields = [
+          'id' => ['type' => 'string', 'length' => 50, 'null' => false],
+          'steam_name' => ['type' => 'string', 'length' => 255, 'null' => false],
+          'email'      => ['type' => 'string', 'length' => 255, 'null' => true],
+          'first_name' => ['type' => 'string', 'length' => 255, 'null' => true],
+          'last_name' => ['type' => 'string', 'length' => 255, 'null' => true],
+          'last_name' => ['type' => 'string', 'length' => 255, 'null' => true],
+          'active'    => ['type' => 'integer', 'length' => 1],
+          'avatar'    => ['type' => 'string', 'length' => 255, 'null' => true],
+          'created'   => 'datetime',
+          'modified'  => ['type' => 'datetime', 'null' => true],
+          '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id']]
+          ]
+      ];
+
+      public $records = [
+          [
+              'id'  => '76561198121209165',
+              'steam_name' => 'Kenobi',
+              'first_name' => 'Pedro',
+              'last_name' => 'Dib',
+              'active' => 1,
+              'avatar'  => 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/5d/5d82427acccb75bc31f945f65dec3dafd50cc0af_full.jpg',
+              'created' => '2018-01-18 10:39:23',
+              'modified' => '2018-03-18 10:41:31'
+          ],
+          [
+              'id'  => '76561198121209165112',
+              'steam_name' => 'Kenobi Error',
+              'created' => '2018-01-18 10:41:23',
+              'modified' => '2018-01-18 10:43:31'
+          ],
+          [
+             'id'  => '765611909165112',
+              'steam_name' => 'Kenobi Error 2',
+              'created' => '2018-01-18 11:41:23',
+              'modified' => '2018-01-18 11:43:31'
+          ]
+      ];
+ }
---
+++ api/tests/TestCase/Controller/PlayerControllerTest.php
@@ -0,0 +1,173 @@
+<?php 
+namespace App\Test\TestCase\Controller;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\IntegrationTestCase;
+
+class PlayerControllerTest extends IntegrationTestCase
+{
+    public $fixtures = ['app.player'];
+
+    public function testLogin()
+    {
+        $data = [
+            'id' => '76561198121209165'
+        ];
+
+        $this->post('/player/login', $data);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("id");
+        $this->assertResponseContains("OK");
+        
+    }
+
+    public function testLoginEmpty()
+    {
+        $data = [];
+
+        $this->post('/player/login', $data);
+
+        $this->assertResponseCode(400);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("NOK");
+        
+    }
+
+    public function testLoginInvalid()
+    {
+        $data = [
+            'id' => '7612120165'
+        ];
+
+        $this->post('/player/login', $data);
+
+        $this->assertResponseCode(404);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("NOK");
+        
+    }
+
+    public function testLoginNew()
+    {
+        $data = [
+            'id' => '76561198303910588'
+        ];
+
+        $this->post('/player/login', $data);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("id");
+        $this->assertResponseContains("OK");
+        
+    }
+
+    public function testEdit()
+    {
+        $data = [
+            'first_name' => 'Pedro',
+            'last_name'  => 'Dib',
+            'email'     => 'pedrohenrique635@hotmail.com'
+        ];
+
+        $this->put('/player/edit/76561198121209165', $data);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("OK");
+    }
+
+    public function testEditEmpty()
+    {
+        $data = [
+            'first_name' => 'Pedro',
+            'last_name'  => 'Dib',
+            'email'     => 'pedrohenrique635@hotmail.com'
+        ];
+
+        $this->put('/player/edit/', $data);
+
+        $this->assertResponseCode(400);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("NOK");
+    }
+
+    public function testEditInvalid()
+    {
+        $data = [
+            'first_name' => 'Pedro',
+            'last_name'  => 'Dib',
+            'email'     => 'pedrohenrique635@hotmail.com'
+        ];
+
+        $this->put('/player/edit/7612120165', $data);
+
+        $this->assertResponseCode(404);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("NOK");
+    }
+
+    public function testUpdateBySteam()
+    {
+        $this->put('/player/updateBySteam/76561198121209165');
+
+        $this->assertResponseCode(200);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("OK");
+        
+    }
+
+    public function testUpdateBySteamEmpty()
+    {
+        $this->put('/player/updateBySteam/');
+
+        $this->assertResponseCode(400);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("NOK");
+        
+    }
+
+    public function testUpdateBySteamInvalid()
+    {
+        $this->put('/player/updateBySteam/7612120165');
+
+        $this->assertResponseCode(404);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("NOK");
+        
+    }
+
+    public function testStats()
+    {
+        $this->get('/player/stats/76561198121209165');
+
+        $this->assertResponseCode(200);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("OK");
+        $this->assertResponseContains("\"stats\":");
+        
+    }
+
+    public function testStatsEmpty()
+    {
+        $this->get('/player/stats/');
+
+        $this->assertResponseCode(400);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("NOK");
+        
+    }
+
+    public function testStatsInvalid()
+    {
+        $this->get('/player/stats/7612120165');
+
+        $this->assertResponseCode(404);
+        $this->assertContentType("application/json");
+        $this->assertResponseContains("NOK");
+        
+    }
+
+}
